17982c3dd2e7552974f0fb4f6c3296315cb4628d
lizhongxiang <lzx@qq.com>
2022-03-02 18:04:05 +0800

    feat:kafka 消费测试

--- run_start.sh
+++ run_start.sh
@@ -1,1 +1,1 @@
-nohup java -Dspring.profiles.active=test -Dserver.port=8088 -jar elk_log_sdk-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
+nohup java -Dspring.profiles.active=test -Dserver.port=8088 -Xms50m -Xmx150m -XX:-UseGCOverheadLimit -jar elk_log_sdk-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
--- src/main/java/com/lzx/elk/log/sdk/ElkLogSdkApplication.java
+++ src/main/java/com/lzx/elk/log/sdk/ElkLogSdkApplication.java
@@ -1,15 +1,29 @@
 package com.lzx.elk.log.sdk;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class ElkLogSdkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElkLogSdkApplication.class, args);
+    }
+
+    @KafkaListener(topics = "sqm")
+    public void listen(String in) {
+        log.info(in);
+    }
+
+
+    @KafkaListener(groupId = "do_action",topics = "sqm")
+    public void doActionListener(String in) {
+        log.info(in);
     }
 
 }
